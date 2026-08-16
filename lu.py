31eb8f1103946a93d36cb6ec96666d591ed73ba7
carmilaswift <contagratuita89@gmail.com>
2026-08-15 22:24:21 -0300

    carmilaswift

---
+++ lu.py
@@ -0,0 +1,7 @@
+echo "# carmilaswift" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/carmilaswift/carmilaswift.git
+git push -u origin main
